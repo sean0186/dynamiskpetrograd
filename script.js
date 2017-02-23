52fa1f6601c7726ca8d6ae22f616b319e4fedaba
Sean Rønholt <Sean Rnholt>
2017-02-23 21:30:19 +0100

    add data_navn, pris and rabatpris

--- script.js
+++ script.js
@@ -21,6 +21,12 @@ function visProdukt(produkt) {
     // klon produkt_template
     var klon = document.querySelector("#produkt_template").content.cloneNode(true);
     // indsæt data i klon
+    klon.querySelector(".data_navn").innerHTML = produkt.navn;
+    klon.querySelector(".data_pris").innerHTML = produkt.pris;
+
+
+    var rabatpris = Math.ceil(produkt.pris - (produkt.pris * produkt.rabatsats / 100));
+    klon.querySelector(".data_rabatpris").innerHTML = rabatpris;
 
     // append klon til .produkt_liste
 
